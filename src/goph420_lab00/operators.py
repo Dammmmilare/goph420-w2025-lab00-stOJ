fc90eb9ce9e88dd8f58ca77efc60bdca8d104dbf
Colin.Maloney <colin.maloney@ucalgary.ca>
2025-01-21 08:28:07 -0700

    subtract function -colin

--- src/goph420_lab00/operators.py
+++ src/goph420_lab00/operators.py
@@ -28,4 +28,19 @@ def multiply(x, y):
     int or float or array_like
     The (element-wise) product of x and y.
     """
-    return x * y
+    return x * y 
+
+def subtract(x, y):
+    """Subtract two numbers or arrays.
+    Parameters
+    ----------
+    x : int or float or array_like
+    The first number to subtract.
+    y : int or float or array_like
+    The second number to subtract.
+    Returns
+    -------
+    int or float or array_like
+    The difference of x and y.
+    """
+    return x - y
